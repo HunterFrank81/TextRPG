c5449a7f5c03f1418858f5b6d4ea6635ba91de8b
HunterFrank81 <frank_jaeger@gmx.de>
2018-01-07 12:28:25 +0100

    Fixed code

--- helper.py
+++ helper.py
@@ -1,6 +1,5 @@
 __author__ = 'Frank'
 
-<<<<<<< HEAD
 from random import randint
 from config import DEBUG
 
@@ -100,24 +99,6 @@ def IsInteger(sel):
 
 ###StringFromList concatenates all entries from a list, separated by a comma. Optionally adds numbering
 def StringFromList(lst, start_index = 0):
-=======
-###pick_from_list takes the task to pick from a list of features
-def pick_from_list(message, lst):
-	selection = 0
-	print message + string_from_list(lst,numbering=True)
-	while selection not in range(1,len(lst)+1):
-		try:
-			selection = int(raw_input("> "))
-		except:
-			print "Please enter a number between 1 and " + str(len(lst))
-		else:
-			if selection not in range(1,len(lst)+1):
-				print "Please enter a number between 1 and " + str(len(lst))
-	return lst[selection-1]
-
-###string_from_list concatenates all entries from a list, separated by a comma. Optionally adds numbering
-def string_from_list(lst, numbering = False):
->>>>>>> origin/master
 	out = ""
 	for index, item in enumerate(lst):
 		if index > 0:
